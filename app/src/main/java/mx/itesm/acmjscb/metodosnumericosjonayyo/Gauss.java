585e45c861585ba5b8ce3e0e10c48363237bb72f
Alejandro <A01370909@itesm.mx>
2017-05-12 19:41:42 -0500

    Gauss completado con formato

--- app/src/main/java/mx/itesm/acmjscb/metodosnumericosjonayyo/Gauss.java
+++ app/src/main/java/mx/itesm/acmjscb/metodosnumericosjonayyo/Gauss.java
@@ -10,20 +10,21 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 
 public class Gauss extends Fragment implements View.OnClickListener {
 
+    private static StringBuilder procedimiento = new StringBuilder(10000);
     private EditText entradaColumna;
     private TextView columnas;
     private Button agregarColumna;
+    private Button calcularGauss;
     private ArrayList<ArrayList<Double>> matriz;
     private Toast toast;
     private int numCoef = 0;
     private int numIncog = 0;
-
-
 
     public Gauss() {
         // Required empty public constructor
@@ -37,7 +38,9 @@ public class Gauss extends Fragment implements View.OnClickListener {
         entradaColumna = (EditText) view.findViewById(R.id.txtEntradaColumnas);
         columnas = (TextView) view.findViewById(R.id.txtColumnasMatriz);
         agregarColumna = (Button) view.findViewById(R.id.btnAgregarColumnas);
+        calcularGauss = (Button) view.findViewById(R.id.btnCalcularGauss);
         agregarColumna.setOnClickListener(this);
+        calcularGauss.setOnClickListener(this);
         matriz = new ArrayList<ArrayList<Double>>();
 
         // Inflate the layout for this fragment
@@ -58,6 +61,16 @@ public class Gauss extends Fragment implements View.OnClickListener {
                     toast.show();
                 }
                 break;
+
+            case R.id.btnCalcularGauss:
+                try{
+                    calcularGauss();
+                    procedimiento = new StringBuilder(10000);
+                }
+                catch (Exception e){
+                    toast = Toast.makeText(view.getContext(),"Error inesperado",Toast.LENGTH_LONG);
+                    toast.show();
+                }
         }
     }
 
@@ -73,28 +86,180 @@ public class Gauss extends Fragment implements View.OnClickListener {
             numIncog = matriz.get(0).size()-2;
         }
         else if(nuevaCol.size() != numCoef){
-            toast = Toast.makeText(getActivity(), "Número de coeficientes erróneo" , Toast.LENGTH_LONG);
+            toast = Toast.makeText(getActivity(), "Metiste mal los coeficientes" , Toast.LENGTH_LONG);
             toast.show();
         } else{
             matriz.add(nuevaCol);
             numIncog--;
         }
-        toast = Toast.makeText(getActivity(),"Ecuaciones faltantes: "+numIncog,Toast.LENGTH_LONG);
+        toast = Toast.makeText(getActivity(),"Te faltan "+numIncog+" ecuaciones/columnas",Toast.LENGTH_LONG);
 
     }
         //faltantes.setText("Ecuaciones faltantes: "+numIncog);
 
 
     public void imprimirColumnas(){
-        String res = "";
-        for(ArrayList<Double> lista: matriz){
+        String res = "La matriz es:\n";
+        /*for(ArrayList<Double> lista: matriz){
             for(Double val: lista){
                 res += val + ",";
             }
+            res += "\n";
+        }*/
+        for(ArrayList<Double> lista: matriz){
+            res += lista.toString();
             res += "\n";
         }
         columnas.setText(res);
     }
 
+
+
+    public void calcularGauss() {
+        procedimiento.append("**** MATRIZ ORIGINAL ****\n");
+        imprimirMatriz(matriz);
+        matriz = gauss(matriz);
+        procedimiento.append("**** MATRIZ FINAL ****\n");
+        imprimirMatriz(matriz);
+        columnas.setText(procedimiento.toString());
+
+    }
+
+    public static ArrayList<ArrayList<Double>> gauss(ArrayList<ArrayList<Double>> matriz){
+        int m = 0;
+        int n = 0;
+        int c = 1;
+        int numFilas = matriz.size();
+
+        for (int fila = 0; fila<numFilas; fila++){
+            // PIVOTE ES CERO
+            if (matriz.get(fila).get(fila) == 0){
+                // SI EL VALOR DEBAJO DEL PIVOTE NO ES CERO
+                for (int k = fila+1; k<numFilas; k++){
+                    if (matriz.get(k).get(fila) != 0){
+                        m = fila;
+                        n = k;
+                        matriz = cambiaFilas(matriz,m,n);
+                        matriz = gaussSimple(matriz,fila);
+                    }
+                }
+            }
+            else{
+                matriz = gaussSimple(matriz,fila);
+            }
+        }
+
+        return matriz;
+
+    }
+
+
+    public static ArrayList<ArrayList<Double>> gaussSimple(ArrayList<ArrayList<Double>> a, int fila){
+        //System.out.println("***NUMERO DE FILA *** "+fila);
+        ArrayList<ArrayList<Double>> mat = a;
+        ArrayList<Double> filaPivote = mat.get(fila);
+        ArrayList<Double> filaDebajoPivote;
+        ArrayList<Double> nuevaFilaDebajoPivote;
+        ArrayList<Double> filaArribaPivote;
+        ArrayList<Double> nuevaFilaArribaPivote;
+        Double pivote = mat.get(fila).get(fila);
+        Double numDebajoPivote;
+        Double numArribaPivote;
+
+        // IGUALAR PIVOTE A 1
+        filaPivote = multiplicarFila(filaPivote,1/pivote);
+        mat.set(fila, filaPivote);
+        procedimiento.append("\n**** DESPUÉS NORMALIZAR PIVOTE ****\n");
+        imprimirMatriz(mat);
+        //System.out.println(mat.size()-fila+" numfilas - filas");
+        //SUSTITUCIÓN HACIA ABAJO
+        procedimiento.append("**** SUSTITUCIÓN HACIA ABAJO ****\n\n");
+        for(int col = 1; col< mat.size()-fila;col++){
+            //System.out.println(fila+col+" fila+col");
+            filaDebajoPivote = mat.get(fila+col);
+            //System.out.println("filaDebajo"+filaDebajoPivote.toString());
+            numDebajoPivote = filaDebajoPivote.get(fila);
+            nuevaFilaDebajoPivote = multiplicarFila(filaPivote,-numDebajoPivote);
+            nuevaFilaDebajoPivote = sumarFila(filaDebajoPivote, nuevaFilaDebajoPivote);
+            //System.out.println("nuevaFila"+nuevaFilaDebajoPivote.toString());
+            mat.set(fila+col, nuevaFilaDebajoPivote);
+            procedimiento.append("****DESPUES ITERACION HACIA ABAJO****\n");
+            imprimirMatriz(mat);
+
+        }
+        procedimiento.append("****DESPUES DE SUSTITUIR HACIA ABAJO*****\n");
+        imprimirMatriz(mat);
+        //SUSTITUCIÓN HACIA ARRIBA
+        //System.out.println("FILA -1 "+(fila-1));
+        procedimiento.append("**** SUSTITUCION HACIA ARRIBA ***\n\n");
+
+        for(int col = 0; col<fila;col++){
+            //System.out.println("COL "+col);
+            filaArribaPivote = mat.get(fila-col-1);
+            //System.out.println("filaArriba"+filaArribaPivote.toString());
+            numArribaPivote = filaArribaPivote.get(fila);
+            nuevaFilaArribaPivote = multiplicarFila(filaPivote,-numArribaPivote);
+            nuevaFilaArribaPivote = sumarFila(filaArribaPivote, nuevaFilaArribaPivote);
+            //System.out.println("nuevaFila"+nuevaFilaArribaPivote.toString());
+            mat.set(fila-col-1, nuevaFilaArribaPivote);
+            procedimiento.append("\n****DESPUES ITERACION HACIA ARRIBA****\n");
+            imprimirMatriz(mat);
+
+        }
+
+        procedimiento.append("****DESPUES DE SUSTITUIR HACIA ARRIBA*****\n");
+        imprimirMatriz(mat);
+        return mat;
+    }
+
+    public static  ArrayList<ArrayList<Double>> cambiaFilas( ArrayList<ArrayList<Double>> a, int filaPivote, int siguiente){
+
+        ArrayList<ArrayList<Double>> temp = new ArrayList<ArrayList<Double>>(a.size());
+        for (ArrayList<Double> fila: a){
+            temp.add(fila);
+        }
+        ArrayList<ArrayList<Double>> mat = a;
+        temp.set(siguiente, mat.get(siguiente));
+        mat.set(siguiente, mat.get(filaPivote));
+        mat.set(filaPivote, temp.get(siguiente));
+        return mat;
+    }
+
+    public static ArrayList<Double> multiplicarFila(ArrayList<Double> fila, Double multi){
+        ArrayList<Double> nuevaFila = new ArrayList<Double>(fila.size());
+        Double nuevoNum;
+        for (Double num: fila){
+            nuevoNum = num*multi;
+            nuevaFila.add(nuevoNum);
+        }
+        return nuevaFila;
+
+    }
+
+    public static ArrayList<Double> sumarFila(ArrayList<Double> fila1, ArrayList<Double> fila2){
+        ArrayList<Double> sumaFila = new ArrayList<Double>(fila1.size());
+        for (int cont = 0; cont < fila1.size(); cont++){
+            sumaFila.add(fila1.get(cont)+fila2.get(cont));
+        }
+        return sumaFila;
+
+    }
+
+    public static void imprimirMatriz(ArrayList<ArrayList<Double>> mat){
+        DecimalFormat df = new DecimalFormat("0.000");
+        String filaBonita = "";
+        for (ArrayList<Double> fila : mat){
+            filaBonita = "[";
+            for (Double num: fila){
+                filaBonita += df.format(num)+",";
+            }
+            filaBonita = filaBonita.substring(0, filaBonita.length()-1);
+            filaBonita +="]";
+            procedimiento.append(filaBonita+"\n");
+            filaBonita = "";
+
+        }
+        procedimiento.append("\n");
+    }
 
 }
